1918feb412f4bf3218996368b06dba10821e2060
zhangcheng <358731817@qq.com>
2017-10-19 14:57:02 +0800

    修改配置文件

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
-    port: 8090,
+    port: 666,
     session: {
       secret: 'tup-ucenter',
       key: 'tup-ucenter',
       maxAge: 2592000000
     },
-    mongodb: 'mongodb://192.168.0.121:27017/tup-ucenter',
+    mongodb: 'mongodb://127.0.0.1:27017/tup-ucenter',
     jwtTokenSecret:'tup123456'
   };
